6fd7ae67959941b877c206445ff4641442367229
Griffin <gmd298@gmail.com>
2022-03-16 12:22:52 -0400

    commit tree 13bb7cda122a9067a26d57469e0dc2e7bedb438c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// const findMatching = drivers.filter(drivers => drivers.'Bobby' && drivers.'Sammy')
+
+// return findMatching
+
+function findMatching(drivers, name) {
+    return drivers.filter((driverName) => { return driverName.toLowerCase() === name.toLowerCase() });
+}
+
+function fuzzyMatch(drivers, letterMatch) {
+    return drivers.filter((driverName) => { return driverName.slice(0, letterMatch.length) === letterMatch });
+}
+
+function matchName(drivers, matching) {
+    return drivers.filter((driverInfo) => {return driverInfo.name === matching})
+}
